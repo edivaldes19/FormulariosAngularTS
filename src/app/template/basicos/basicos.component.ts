2ff7bce2ddfce91d2ad7d0360648db7c9331e9af
edivaldes19 <recio-manuel@outlook.com>
2022-11-25 14:06:47 -0600

    Template-based forms.

---
+++ src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core'
+import { NgForm } from '@angular/forms'
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html'
+})
+export class BasicosComponent {
+  @ViewChild('miFormulario') miFormulario!: NgForm
+  public initForm = {
+    producto: 'RTX 4080ti',
+    precio: 10,
+    existencias: 10
+  }
+  nombreValido(): boolean {
+    return this.miFormulario?.form.controls['producto']?.invalid && this.miFormulario?.form.controls['producto']?.touched
+  }
+  precioValido(): boolean {
+    return this.miFormulario?.form.controls['precio']?.touched && this.miFormulario?.form.controls['precio']?.value < 0
+  }
+  guardar() {
+    this.miFormulario.resetForm({
+      producto: 'Donitas',
+      precio: 0,
+      existencias: 0
+    })
+  }
+}
---
+++ src/app/template/switches/switches.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+@Component({
+  selector: 'app-switches',
+  templateUrl: './switches.component.html'
+})
+export class SwitchesComponent {
+  public persona = {
+    genero: 'F',
+    notificaciones: true,
+  }
+  public terminosYCondiciones: boolean = false
+}
